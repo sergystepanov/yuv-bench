afe679f82603215997a37bdcfcf85745ed3d83f3
sergystepanov <sergystepanov@gmail.com>
2021-03-15 02:15:01 +0300

    Color stats

--- main.c
+++ main.c
@@ -2,9 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <string.h>
+#include <float.h>
 
 #include "conv.h"
 #include "gen.h"
+
+#if defined(_WIN32) || defined(_WIN64)
+const char *os = "win";
+#else
+#ifdef __linux
+const char* os = "nix";
+#else
+const char* os = "";
+#endif
+#endif
 
 #define RED   "\x1B[31m"
 #define GRN   "\x1B[32m"
@@ -17,8 +29,8 @@
 #define RESET "\x1B[0m"
 
 int main(void) {
-	const int w = 2000;
-	const int h = 1000;
+    const int w = 1920;
+    const int h = 1080;
 
     const int ss = 4 * w * h;
     const int ds = w * h * 3 / 2;
@@ -26,16 +38,19 @@ int main(void) {
     unsigned char *dst = malloc(ds * sizeof(unsigned char));
 
     // cpu
-    system("cat /proc/cpuinfo | grep \"model name\" | head -1");
-    printf("image: %dx%d\nsrc: %d bytes, dst: %d bytes\n\n", w, h, ss, ds);
+    if (strcmp(os, "win") != 0) {
+        system("cat /proc/cpuinfo | grep \"model name\" | head -1");
+    } else {
+        system("for /f \"usebackq skip=1 tokens=*\" %i in (`wmic cpu get name ^| findstr /r /v \"^$\"`) do @echo %i");
+    }
+    printf("image: %dx%d\nsrc: %dKB, dst: %dKB\n\n", w, h, ss / 1000, ds / 1000);
 
     some_rgba(w, h, src);
 
     clock_t start, end;
-    double wasted;
-    double mean = 0;
-    int runs = 100;
-    double vals[100];
+    double wasted, mean = 0, min = DBL_MAX, max = 0;
+    int runs = 200;
+    double vals[200];
 
     for (int i = 1; i <= runs; i++) {
         start = clock();
@@ -43,10 +58,13 @@ int main(void) {
         end = clock();
         wasted = (double) (end - start) / CLOCKS_PER_SEC * 1000;
         mean += (wasted - mean) / i;
-        printf("| %5.2f", wasted);
-        printf(GRN "ms " RESET);
-        if (i % 6 == 0) printf("\n");
         vals[i - 1] = wasted;
+        if (wasted < min) {
+            min = wasted;
+        }
+        if (wasted > max) {
+            max = wasted;
+        }
     }
 
     double sum = 0;
@@ -54,7 +72,31 @@ int main(void) {
         sum += pow((vals[i] - mean), 2);
     }
     double std_deviation = sqrt(sum / runs);
-    printf("\n------------\n");
+    double sigma = (/*min + max*/mean);
+
+    for (int i = 0; i < runs; i++) {
+        if (vals[i] == max) {
+            printf(RED);
+        } else {
+            if (vals[i] == min) {
+                printf(GRN);
+            } else {
+                if (vals[i] <= sigma + std_deviation) {
+                    printf(GRN);
+                } else {
+                    if (vals[i] > sigma + std_deviation) {
+                        printf(YEL);
+                    }
+                }
+            }
+        }
+
+        printf("%6.2f", vals[i]);
+        //printf(GRN "ms " RESET);
+        if ((i + 1) % 10 == 0) printf("\n");
+    }
+
+    printf(RESET "\n------------\n");
     printf(BOLD "%.2fms" RESET, mean);
     printf(" (+-%.2fms)\n", std_deviation);
 
